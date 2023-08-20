5d55f3fb18d17d1393b7c49085270cf4fdf8bd53
syedfaheem27 <syedfaheem2719@gmail.com>
2023-08-20 23:48:28 +0530

    Add AuthContext to implement a fake authentication set up while logging in

---
+++ src/contexts/AuthContext.jsx
@@ -0,0 +1,60 @@
+import { createContext, useReducer } from "react";
+
+const FAKE_USER = {
+  name: "Jack",
+  email: "jack@example.com",
+  password: "qwerty",
+  avatar: "https://i.pravatar.cc/100?u=zz",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "login":
+      return {
+        ...state,
+        isAuthenticated: true,
+        user: action.payload,
+      };
+
+    case "logout":
+      return {
+        ...state,
+        isAuthenticated: false,
+        user: null,
+      };
+
+    default:
+      throw new Error("Unknown action");
+  }
+}
+
+const AuthContext = createContext();
+
+export default function AuthProvider({ children }) {
+  const [{ isAuthenticated, user }, dispatch] = useReducer(reducer, {
+    isAuthenticated: false,
+    user: null,
+  });
+
+  function login(email, password) {
+    if (email === FAKE_USER.email && password === FAKE_USER.password)
+      dispatch({ type: "login", payload: FAKE_USER });
+  }
+
+  function logout() {
+    dispatch({ type: "logout" });
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isAuthenticated,
+        user,
+        login,
+        logout,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
